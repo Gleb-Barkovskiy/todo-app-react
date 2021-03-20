9083fca6d8c9574d888d91b33914f39abfb1a36d
Глеб Барковский <gleb@MacBook-Pro-Gleb.local>
2021-03-20 16:45:15 +0300

    bugfix

--- src/components/WeekTodoLists/WeekTodoList.jsx
+++ src/components/WeekTodoLists/WeekTodoList.jsx
@@ -2,15 +2,14 @@ import TodoField from "../TodoField/TodoField";
 import './styles.css';
 
 const WeekTodoList = () => {
+    const days = [
+        "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"
+    ];
     return (
         <div className="week-todo-wrapper">
-            <TodoField day="Понедельник"/>
-            <TodoField day="Вторник"/>
-            <TodoField day="Среда"/>
-            <TodoField day="Четверг"/>
-            <TodoField day="Пятница"/>
-            <TodoField day="Суббота"/>
-            <TodoField day="Воскресенье"/>
+            {days.map(day => 
+                <TodoField day={day}/>
+            )}
         </div>
     )
 }
